94da9d8362ea9d43c982869ab81a1c4151cb5808
warfdev <129093003+warfdev@users.noreply.github.com>
2023-10-10 00:40:40 +0300

    Create runclient.bat

---
+++ runclient.bat
@@ -0,0 +1,12 @@
+@echo off
+color a
+echo CLIENT BY (DISCORD) zmochas / ksvnwharf
+echo BOTU BASLATMAK ICIN HERHANGI BIR DUGMEYE BAS
+pause >nul
+cls
+echo BOT BASLATILIYOR...
+color 7
+node index.js
+echo BOT BASLATILDI.
+color 7
+pause
