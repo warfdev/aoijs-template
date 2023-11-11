a5e775debf88c0be933ee3bdd4924fccdfafc1c9
warfdev <129093003+warfdev@users.noreply.github.com>
2023-11-11 21:51:37 +0300

    Update runclient.js

--- runclient.js
+++ runclient.js
@@ -1,14 +1,28 @@
 // runclient for android
 
 const { execSync } = require('child_process');
+const config = require("./config.js");
+const chalk = require("chalk");
 
-console.log('CLIENT BY (DISCORD) zmochas / ksvnwharf');
-console.log('BOTU BASLATMAK ICIN HERHANGI BIR DUGMEYE BAS');
-await new Promise(resolve => {
-    process.stdin.once('data', () => resolve());
-});
+// sistem degisti
+// console.log('CLIENT BY (DISCORD) zmochas / ksvnwharf');
+// console.log('BOTU BASLATMAK ICIN HERHANGI BIR DUGMEYE BAS');
 
-console.clear();
-console.log('BOT BASLATILIYOR...');
-execSync('node index.js', { stdio: 'inherit' });
-console.log('BOT BASLATILDI.');
+async function startBot() {
+    return new Promise(resolve => {
+        process.stdin.once('data', () => resolve());
+    });
+}
+
+async function main() {
+    console.clear();
+    console.log(chalk.green.bgBlack.bold("[*] ") + chalk.white.bgBlack.dim("Console - Client RunSC"));
+    console.log(chalk.green.bgBlack.bold("[*] ") + chalk.magentaBright.bgBlack.bold("Client'i aktif etmek icin herhangi bir mesaj gönder."));
+    await startBot();
+    console.clear();
+    console.log(chalk.green.bgBlack.bold("[*] ") + chalk.white.bgBlack.dim("Console - Client RunSC"));
+    console.log(chalk.green.bgBlack.bold("[*] ") + chalk.magentaBright.bgBlack.bold("Client aktif ediliyor..."));
+    execSync('node index.js', { stdio: 'inherit' });
+}
+
+main(); // run
